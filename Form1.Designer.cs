b5110ae9604e36b48e78cff2d9e02370589a04fa
Konrad Goliński <93205811+i13k@users.noreply.github.com>
2026-06-21 09:16:10 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace OA2CertTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOemID = new System.Windows.Forms.Label();
+            this.lblChecksum = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTableID = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDataCRC = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblRevision = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblCreator = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblCreatorRev = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblCertStatus = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblOAVersion = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(55, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "OEM ID:";
+            // 
+            // lblOemID
+            // 
+            this.lblOemID.AutoSize = true;
+            this.lblOemID.Font = new System.Drawing.Font("Segoe UI Semibold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblOemID.Location = new System.Drawing.Point(127, 11);
+            this.lblOemID.Name = "lblOemID";
+            this.lblOemID.Size = new System.Drawing.Size(75, 21);
+            this.lblOemID.TabIndex = 1;
+            this.lblOemID.Text = "(OEM ID)";
+            // 
+            // lblChecksum
+            // 
+            this.lblChecksum.AutoSize = true;
+            this.lblChecksum.Font = new System.Drawing.Font("Segoe UI Semibold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblChecksum.ForeColor = System.Drawing.Color.Crimson;
+            this.lblChecksum.Location = new System.Drawing.Point(127, 53);
+            this.lblChecksum.Name = "lblChecksum";
+            this.lblChecksum.Size = new System.Drawing.Size(94, 21);
+            this.lblChecksum.TabIndex = 3;
+            this.lblChecksum.Text = "(Checksum)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(54, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Table ID:";
+            // 
+            // lblTableID
+            // 
+            this.lblTableID.AutoSize = true;
+            this.lblTableID.Font = new System.Drawing.Font("Segoe UI Semibold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTableID.Location = new System.Drawing.Point(127, 32);
+            this.lblTableID.Name = "lblTableID";
+            this.lblTableID.Size = new System.Drawing.Size(117, 21);
+            this.lblTableID.TabIndex = 5;
+            this.lblTableID.Text = "(OEM Table ID)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(36, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Checksum:";
+            // 
+            // lblDataCRC
+            // 
+            this.lblDataCRC.AutoSize = true;
+            this.lblDataCRC.Font = new System.Drawing.Font("Segoe UI Semibold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblDataCRC.Location = new System.Drawing.Point(127, 137);
+            this.lblDataCRC.Name = "lblDataCRC";
+            this.lblDataCRC.Size = new System.Drawing.Size(87, 21);
+            this.lblDataCRC.TabIndex = 7;
+            this.lblDataCRC.Text = "(Data CRC)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(42, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 21);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Data CRC:";
+            // 
+            // lblRevision
+            // 
+            this.lblRevision.AutoSize = true;
+            this.lblRevision.Font = new System.Drawing.Font("Segoe UI Semibold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblRevision.Location = new System.Drawing.Point(127, 74);
+            this.lblRevision.Name = "lblRevision";
+            this.lblRevision.Size = new System.Drawing.Size(81, 21);
+            this.lblRevision.TabIndex = 9;
+            this.lblRevision.Text = "(Revision)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(49, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 21);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Revision:";
+            // 
+            // lblCreator
+            // 
+            this.lblCreator.AutoSize = true;
+            this.lblCreator.Font = new System.Drawing.Font("Segoe UI Semibold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblCreator.Location = new System.Drawing.Point(127, 95);
+            this.lblCreator.Name = "lblCreator";
+            this.lblCreator.Size = new System.Drawing.Size(75, 21);
+            this.lblCreator.TabIndex = 11;
+            this.lblCreator.Text = "(Creator)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(56, 95);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 21);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Creator:";
+            // 
+            // lblCreatorRev
+            // 
+            this.lblCreatorRev.AutoSize = true;
+            this.lblCreatorRev.Font = new System.Drawing.Font("Segoe UI Semibold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblCreatorRev.Location = new System.Drawing.Point(127, 116);
+            this.lblCreatorRev.Name = "lblCreatorRev";
+            this.lblCreatorRev.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblCreatorRev.Size = new System.Drawing.Size(106, 21);
+            this.lblCreatorRev.TabIndex = 13;
+            this.lblCreatorRev.Text = "(Creator Rev)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(26, 116);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 21);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Creator Rev:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(12, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 46);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Install";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(130, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 46);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Save...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCertStatus
+            // 
+            this.lblCertStatus.AutoSize = true;
+            this.lblCertStatus.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblCertStatus.Location = new System.Drawing.Point(127, 179);
+            this.lblCertStatus.Name = "lblCertStatus";
+            this.lblCertStatus.Size = new System.Drawing.Size(104, 21);
+            this.lblCertStatus.TabIndex = 19;
+            this.lblCertStatus.Text = "(Cert Status)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(38, 179);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(83, 21);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Certificate:";
+            // 
+            // lblOAVersion
+            // 
+            this.lblOAVersion.AutoSize = true;
+            this.lblOAVersion.Font = new System.Drawing.Font("Segoe UI Semibold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblOAVersion.Location = new System.Drawing.Point(127, 158);
+            this.lblOAVersion.Name = "lblOAVersion";
+            this.lblOAVersion.Size = new System.Drawing.Size(101, 21);
+            this.lblOAVersion.TabIndex = 21;
+            this.lblOAVersion.Text = "(OA Version)";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.Location = new System.Drawing.Point(30, 158);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(91, 21);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "OA Version:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 266);
+            this.Controls.Add(this.lblOAVersion);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblCertStatus);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblCreatorRev);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblCreator);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblRevision);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblDataCRC);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblTableID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblChecksum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblOemID);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "OA 2.x Utility";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOemID;
+        private System.Windows.Forms.Label lblChecksum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTableID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDataCRC;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblRevision;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblCreator;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblCreatorRev;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblCertStatus;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblOAVersion;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Drawing;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OA2CertTool
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern uint GetSystemFirmwareTable(uint provider, uint tableId, IntPtr buffer, uint bufferSize);
+
+        // 0x41435049 == 'ACPI', 0x43494C53 = 'CILS' = 'SLIC' in little endian
+        private const uint ACPI_ID = 0x41435049;
+        private const uint SLIC_ID = 0x43494C53;
+        private string dataCrc;
+        private byte[] cert;
+        public static byte[] GetSLICTable()
+        {
+            if (GetSystemFirmwareTable(ACPI_ID, SLIC_ID, IntPtr.Zero, 0) != 374)
+            {
+                throw new Exception("ACPI SLIC table not found or has invalid length!");
+            }
+
+            IntPtr buffer = Marshal.AllocHGlobal(374);
+            if (GetSystemFirmwareTable(ACPI_ID, SLIC_ID, buffer, 374) == 0)
+            {
+                throw new Exception("Failed to read ACPI SLIC table!");
+            }
+
+            byte[] data = new byte[374];
+            Marshal.Copy(buffer, data, 0, 374);
+
+            return data;
+        }
+        public static bool ValidateChecksum(byte[] acpiTable)
+        {
+            byte sum = 0;
+            for (int i = 0; i < acpiTable.Length; i++) sum += acpiTable[i];
+            return sum == 0;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] SLIC = GetSLICTable();
+
+                Crc32 crc = new Crc32();
+                uint crc32 = crc.Append(0, SLIC, 10, 6);
+                crc32 = crc.Append(crc32, SLIC, 60, 132);
+                dataCrc = crc32.ToString("X8");
+
+                lblOemID.Text = Encoding.ASCII.GetString(SLIC, 10, 6);
+                lblTableID.Text = Encoding.ASCII.GetString(SLIC, 16, 8);
+                lblDataCRC.Text = dataCrc;
+                lblRevision.Text = BitConverter.ToUInt32(SLIC, 24).ToString();
+                lblCreator.Text = Encoding.ASCII.GetString(SLIC, 28, 4);
+                lblCreatorRev.Text = BitConverter.ToUInt32(SLIC, 32).ToString();
+                lblOAVersion.Text = BitConverter.ToUInt16(SLIC, 228).ToString() + "." + BitConverter.ToUInt16(SLIC, 226).ToString();
+
+                if (!ValidateChecksum(SLIC))
+                {
+                    lblChecksum.Text = "Invalid";
+                    lblChecksum.ForeColor = Color.Crimson;
+                }
+                else
+                {
+                    lblChecksum.Text = "OK";
+                    lblChecksum.ForeColor = Color.ForestGreen;
+                }
+
+                cert = (byte[])Properties.Resources.ResourceManager.GetObject(dataCrc);
+                if (cert == null)
+                {
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    lblCertStatus.Text = "Not Found";
+                    lblCertStatus.ForeColor = Color.Crimson;
+                }
+                else
+                {
+                    lblCertStatus.Text = "Found";
+                    lblCertStatus.ForeColor = Color.ForestGreen;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ManagementClass SPP = new ManagementClass("SoftwareLicensingService");
+                ManagementObjectCollection instances = SPP.GetInstances();
+                if (instances.Count != 1)
+                {
+                    MessageBox.Show("More than one SPP instance running!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                foreach (ManagementObject instance in instances)
+                {
+                    instance.InvokeMethod("InstallLicense", new[] { Encoding.ASCII.GetString(cert) });
+                }
+                MessageBox.Show("Certificate installed!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog d = new SaveFileDialog();
+            System.IO.Stream s;
+            d.Filter = "XrML Digital License (*.xrm-ms)|*.xrm-ms";
+            if (d.ShowDialog() == DialogResult.OK)
+            {
+                if ((s = d.OpenFile()) != null)
+                {
+                    s.Write(cert, 0, cert.Length);
+                    s.Close();
+                    MessageBox.Show("Certificate saved!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } else
+                {
+                    MessageBox.Show("Failed to save file!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
